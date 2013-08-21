e574bfbd8fee57b60d6a70a91c26e95e4c91c7dd
Perlover <perlover@perlover.com>
2013-08-21 19:10:16 +0200

    Added some docs. Module is not writtem yet. ;-)

--- lib/DBIx/TxnPool.pm
+++ lib/DBIx/TxnPool.pm
@@ -43,13 +43,13 @@ each statements. It can be very slowly if you update 100,000 and more rows for
 example. The better way to wrap some insert/delete/update statements in one
 transaction for example. But there can be other problem - deadlocks. If a
 deadlock occur the DBI's module can throws exceptions and ideal way to repeat SQL
-statements. This module helps to make it. It has a pool inside for data (FIFO
+statements again. This module helps to make it. It has a pool inside for data (FIFO
 buffer) and calls your callbacks for each pushed item. When you feed a module by
-your data, it wrap data in one transaction up to maximum defined size of up to
-finish method. If deadlock occurs it repeat your callbacks for every item again.
+your data, it wraps data in one transaction up to the maximum defined size or up to
+the finish method. If deadlock occurs it repeats your callbacks for every item again.
 You can define a second callback which will be executed for every item after
 wrapped transaction. For example there can be non-SQL statements, for example a
-deleting files, cleanus and etc.
+deleting files, cleanups and etc.
 
 =head1 CONSTRUCTOR
 
@@ -79,17 +79,42 @@ There are all parameters.
 
 =over parameters
 
-=item B<Required>: transaction item callback
+=item B<Required>: txn_item
 
-=item B<Optional>: post transaction item callback
+The transaction's item callback. Here should be SQL statements and code should
+be safe for repeating (when a deadlock occurs). The C<$_> consists a current item.
+You can modify it if one is hashref for example.
+
+=item B<Optional>: txn_post_item
+
+The post transaction item callback. This code will be executed once for each
+item (defined in C<$_>). It is located outside of the transaction. And it will
+be called if whole transaction was succaessful.
 
 =item B<Required>: dbh
 
-The dbh to be needed for begin_work & commit for wrapping to transaction.
+The dbh to be needed for begin_work & commit method (wrap in a transaction).
 
 =item B<Optional>: size
 
 The size of pool when a commit method will be called when feeding reaches the same size.
+
+=back
+
+=head1 METHODS
+
+=over
+
+=item add
+
+You can add item of data to the pool. This method makes a wrap to transaction.
+It can finish transaction if pool reaches up to size or can repeat a whole
+transaction again if deadlock exception was thrown. The size of transaction may
+be less than your defined size!
+
+=item finish
+
+It makes a final transaction if pool is not empty.
 
 =back
 
